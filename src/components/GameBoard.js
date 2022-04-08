6e806b8fb076a217f697fcd402108cff9ec8f3bb
Alex Beers <alexbeers92@gmail.com>
2022-04-08 17:55:25 -0400

    Adds patch request

--- src/components/GameBoard.js
+++ src/components/GameBoard.js
@@ -8,6 +8,10 @@ const GameBoard = () => {
     const [gameBoard, setGameBoard] = useState({
         cells: []
     })
+
+    const [currentPlayer, setCurrentPlayer] = useState('x')
+
+    const changePlayer = () => currentPlayer === 'x' ? setCurrentPlayer('o') : setCurrentPlayer('x')
     
     const onNewGameClick = () => {
         axios({
@@ -20,10 +24,20 @@ const GameBoard = () => {
 
     const onCellClick = (cell) => {
         console.log(cell)
-
-        if (cell.value === '') {
-            cell.value = 'x'
+        const newCellData = {
+            id: cell._id,
+            row: cell.row,
+            value: currentPlayer
         }
+
+        axios({
+            url: `http://localhost:4741/games/${gameBoard._id}`,
+            method: 'PATCH',
+            data: newCellData
+        })
+            .then((response) => setGameBoard(response.data.game))
+            .then(changePlayer)
+            .catch(console.error)
     }
 
     return (
@@ -34,7 +48,7 @@ const GameBoard = () => {
                     <Row key={rowIndex}>
                         {row.map(cell => (
                             <Cell 
-                                key={cell.position}
+                                key={cell._id}
                                 cell={cell}
                                 onCellClick={() => onCellClick(cell)}
                             />
