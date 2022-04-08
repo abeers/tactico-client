8cb4b164498e0206293f22a5166d38d3a026e70e
Alex Beers <alexbeers92@gmail.com>
2022-04-08 19:24:24 -0400

    Add some displays for current turn and winning

--- src/components/GameBoard.js
+++ src/components/GameBoard.js
@@ -6,7 +6,9 @@ import Cell from './Cell.js'
 
 const GameBoard = () => {
     const [gameBoard, setGameBoard] = useState({
-        cells: []
+        cells: [],
+        winner: '',
+        isOver: false
     })
 
     const [currentPlayer, setCurrentPlayer] = useState('x')
@@ -23,42 +25,49 @@ const GameBoard = () => {
     }
 
     const onCellClick = (cell) => {
-        console.log(cell)
-        const newCellData = {
-            id: cell._id,
-            row: cell.row,
-            value: currentPlayer
-        }
+        if (!gameBoard.isOver && cell.value === '') {
+            const newCellData = {
+                id: cell._id,
+                row: cell.row,
+                value: currentPlayer,
+            }
 
-        axios({
-            url: `http://localhost:4741/games/${gameBoard._id}`,
-            method: 'PATCH',
-            data: newCellData
-        })
-            .then((response) => setGameBoard(response.data.game))
-            .then(changePlayer)
-            .catch(console.error)
+            axios({
+                url: `http://localhost:4741/games/${gameBoard._id}`,
+                method: 'PATCH',
+                data: newCellData,
+            })
+                .then((response) => setGameBoard(response.data.game))
+                .then(changePlayer)
+                .catch(console.error)
+        }
+        
     }
 
     return (
-        <>
-            <p>Testing GameBoard</p>
-            <Container>
-                {gameBoard.cells.map((row, rowIndex) => (
-                    <Row key={rowIndex}>
-                        {row.map(cell => (
-                            <Cell 
-                                key={cell._id}
-                                cell={cell}
-                                onCellClick={() => onCellClick(cell)}
-                            />
-                        ))}
-                    </Row>
-                ))}
-            </Container>
-            <Button onClick={onNewGameClick}>New Game</Button>
-        </>
-    )
+			<>
+				{gameBoard.isOver ? (
+					<p>{gameBoard.winner}, you've won!</p>
+				) : (
+					<p>{currentPlayer}, it's your turn!</p>
+				)}
+
+				<Container>
+					{gameBoard.cells.map((row, rowIndex) => (
+						<Row key={rowIndex}>
+							{row.map((cell) => (
+								<Cell
+									key={cell._id}
+									cell={cell}
+									onCellClick={() => onCellClick(cell)}
+								/>
+							))}
+						</Row>
+					))}
+				</Container>
+				<Button onClick={onNewGameClick}>New Game</Button>
+			</>
+		)
 }
 
 export default GameBoard
